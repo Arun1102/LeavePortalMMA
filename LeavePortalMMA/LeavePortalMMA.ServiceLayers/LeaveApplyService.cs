281ee83962896562282f559e8d6b3fea2683a372
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-07-04 12:54:35 +0800

    ServiceLayer commit version 1

---
+++ LeavePortalMMA/LeavePortalMMA.ServiceLayers/LeaveApplyService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeavePortalMMA.DomainModels;
+using LeavePortalMMA.Repositories;
+using LeavePortalMMA.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace LeavePortalMMA.ServiceLayers
+{
+    public interface ILeaveApplyService
+    {
+        void ApplyLeave(NewLeaveApplyViewModel qvm);
+        void UpdateLeaveDetails(EditLeaveApplyViewModel qvm);
+        void UpdateLeaveCount(int qid, int value);
+        void DeleteLeave(int qid);
+        List<LeaveListViewModel> GetAllLeave();
+        LeaveListViewModel GetLeaveByLeaveID(int LeaveID, int UserID);
+    }
+    public class LeaveApplyService:ILeaveApplyService
+    {
+        ILeaveRepository qr;
+
+        public LeaveApplyService()
+        {
+            qr = new LeaveRepository();
+        }
+
+        public void ApplyLeave(NewLeaveApplyViewModel qvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<NewLeaveApplyViewModel, Leave>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Leave q = mapper.Map<NewLeaveApplyViewModel, Leave>(qvm);
+            qr.ApplyLeave(q); 
+        }
+
+        public void UpdateLeaveDetails(EditLeaveApplyViewModel qvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditLeaveApplyViewModel, Leave>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Leave q = mapper.Map<EditLeaveApplyViewModel, Leave>(qvm);
+            qr.UpdateLeaveDetails(q);
+        }
+
+        public void UpdateLeaveCount(int qid, int value)
+        {
+            qr.UpdateLeaveCount(qid, value);
+        }
+        public void DeleteLeave(int qid)
+        {
+            qr.DeleteLeave(qid);
+
+        }
+        
+
+        public List<LeaveListViewModel> GetAllLeave()
+        {
+            List<Leave> q = qr.GetAllLeave();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Leave, LeaveListViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<LeaveListViewModel> qvm = mapper.Map<List<Leave>, List<LeaveListViewModel>>(q);
+            return qvm;
+        }
+
+        public LeaveListViewModel GetLeaveByLeaveID(int LeaveID, int UserID)
+        {
+            Leave q = qr.GetLeaveByLeaveID(LeaveID).FirstOrDefault();
+            
+            
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Leave, LeaveListViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            LeaveListViewModel qvm = mapper.Map<Leave, LeaveListViewModel>(q);
+
+               
+            return qvm;
+        }
+    }
+}
---
+++ LeavePortalMMA/LeavePortalMMA.ServiceLayers/UserService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeavePortalMMA.DomainModels;
+using LeavePortalMMA.Repositories;
+using LeavePortalMMA.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace LeavePortalMMA.ServiceLayers
+{
+    public interface IUsersService
+    {
+        void UpdateUserDetails(EditUserViewModel uvm);
+        void UpdateUserPassword(EditUserPasswordViewModel uvm);
+        void DeleteUser(int uid);
+        List<UserViewModel> GetUsers();
+        UserViewModel GetUsersByEmailandPassword(string Email, string Password);
+        UserViewModel GetUsersByEmail(string Email);
+        UserViewModel GetUsersByUserID(int UserID);
+    }
+    public class UsersService : IUsersService
+    {
+        IUsersRepository ur;
+
+        public UsersService()
+        {
+            ur = new UsersRepository();
+        }
+
+
+        public void UpdateUserDetails(EditUserViewModel uvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditUserViewModel, Users>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Users u = mapper.Map<EditUserViewModel, Users>(uvm);
+            ur.UpdateUserDetails(u);
+        }
+
+        public void UpdateUserPassword(EditUserPasswordViewModel uvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditUserPasswordViewModel, Users>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Users u = mapper.Map<EditUserPasswordViewModel, Users>(uvm);
+            u.PasswordHash = SHA256HashGenerator.GenerateHash(uvm.Password);
+            ur.UpdateUserPassword(u);
+        }
+
+        public void DeleteUser(int uid)
+        {
+            ur.DeleteUser(uid);
+        }
+
+        public List<UserViewModel> GetUsers()
+        {
+            List<Users> u = ur.GetUsers();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Users, UserViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<UserViewModel> uvm = mapper.Map<List<Users>, List<UserViewModel>>(u);
+            return uvm;
+        }
+
+        public UserViewModel GetUsersByEmailandPassword(string Email, string Password)
+        {
+            Users u = ur.GetUsersByEmailAndPassword(Email, SHA256HashGenerator.GenerateHash(Password)).FirstOrDefault();
+            UserViewModel uvm = null;
+            if (u != null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Users, UserViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                uvm = mapper.Map<Users, UserViewModel>(u);
+            }
+            return uvm;
+        }
+
+        public UserViewModel GetUsersByEmail(string Email)
+        {
+            Users u = ur.GetUsersByEmail(Email).FirstOrDefault();
+            UserViewModel uvm = null;
+            if (u != null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Users, UserViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                uvm = mapper.Map<Users, UserViewModel>(u);
+            }
+            return uvm;
+        }
+
+
+        public UserViewModel GetUsersByUserID(int UserID)
+        {
+            Users u = ur.GetUsersByUserID(UserID).FirstOrDefault();
+            UserViewModel uvm = null;
+            if (u != null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Users, UserViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                uvm = mapper.Map<Users, UserViewModel>(u);
+            }
+            return uvm;
+        }
+
+
+    }
+}
