fb1a2a3eae1c800c4cc601228996b8d4d67b50b5
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-07-07 21:57:56 +0800

    unityconfig commit version1

--- LeavePortalMMA/LeavePortalMMA/Controllers/HomeController.cs
+++ LeavePortalMMA/LeavePortalMMA/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeavePortalMMA.ServiceLayers;
+using LeavePortalMMA.ViewModels;
 
 namespace LeavePortalMMA.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        ILeaveApplyService ls;
+
+        public HomeController(ILeaveApplyService ls)
+        {
+            this.ls = ls;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<LeaveListViewModel> leaves = this.ls.GetAllLeave().Take(10).ToList();
+            return View(leaves);
         }
     }
 }
